de9c62f75e1c610e7925457951cf121d463d6d45
Minjeong Jang <minjeongg24@gmail.com>
2021-11-21 19:21:14 +0000

    back to master

--- main.js
+++ main.js
@@ -6,3 +6,27 @@ toggleBtn.addEventListener('click', () => {
   menu.classList.toggle('active');
   icons.classList.toggle('active');
 });
+
+// const btnOnScroll = document.querySelector('.btn');
+
+// const initUpdateBtnOnScroll = () => {
+//   const btn = document.querySelector('.btnTop');
+//   if(btn) {
+//     window.addEventListener('scroll', () => {
+//       if(window.scrollY >= window.innerHeight) {
+//         btn.classList.add('scroll');
+//       } else {
+//         btn.classList.remove('scroll');
+//       }
+//     });
+//   }
+// }
+
+// function scroll() {
+//   const toTop = document.getElementById('btnTop');
+//   window.scrollY > 0 ? toTop.style.display = 'Block' : toTop.style.display = 'none';
+// }
+
+const showBtn = () => document.querySelector('.btnTop').classList.add('visible');
+const hideBtn = () => document.querySelector('.btnTop').classList.remove('visible');
+document.addEventListener('scroll', (e) => window.scrollY < 100 ? hideBtn() : showBtn());
